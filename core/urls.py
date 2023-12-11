c865370daebe7eebfb90e6c82270ab8f44a91e61
Ana el <95952291+nokixty@users.noreply.github.com>
2023-12-11 08:30:06 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,28 +1,16 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reserva.urls')),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = 'registration/password-reset-form.html'), name = 'reset_password'),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name = 'registration/password-reset-done.html'), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'registration/password-reset-confirm.html', success_url=reverse_lazy('login')), name = 'password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'), 
 ]
 
 if settings.DEBUG:
